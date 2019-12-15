f071fc8d0a382b710c0ebbd05f66baaf070ad965
SimplexShotz <brancy280@gmail.com>
2019-12-14 20:43:13 -0500

    Created the site template (header-main and header-sticky)

--- javascript.js
+++ javascript.js
@@ -1,1 +1,7 @@
-alert("Welcome!");
+
+const observer = new IntersectionObserver(
+  ([e]) => e.target.toggleAttribute("stuck", e.intersectionRatio < 1),
+  {threshold: [1]}
+);
+
+observer.observe(document.getElementsByClassName("header-sticky")[0]);
