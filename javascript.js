215eafb914efa16cea5596d1a099bc3352d6b736
SimplexShotz <44213379+SimplexShotz@users.noreply.github.com>
2019-12-16 10:36:20 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,7 +1,11 @@
-
-const observer = new IntersectionObserver(
-  ([e]) => e.target.toggleAttribute("stuck", e.intersectionRatio < 1),
-  {threshold: [1]}
-);
-
-observer.observe(document.getElementsByClassName("header-sticky")[0]);
+if (!('IntersectionObserver' in window) ||
+    !('IntersectionObserverEntry' in window) ||
+    !('intersectionRatio' in window.IntersectionObserverEntry.prototype)) {
+    document.getElementsByClassName("header-sticky")[0].toggleAttribute("stuck");
+} else {
+  const observer = new IntersectionObserver(
+    ([e]) => e.target.toggleAttribute("stuck", e.intersectionRatio < 1),
+    {threshold: [1]}
+  );
+  observer.observe(document.getElementsByClassName("header-sticky")[0]);
+}
